8f1c383287615293bb565a5c58b4d4bdaf49007d
Gabriel Carraro <gacarraro@gmail.com>
2024-09-09 12:33:59 -0300

    back to single image

--- monitoring_stack/run.sh
+++ monitoring_stack/run.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/with-contenv bashio
 
-# Ensure Docker is running
-if ! pgrep -f docker > /dev/null; then
-    echo "Docker is not running. Starting Docker..."
-    dockerd &
-    # Wait for Docker to start
-    while ! docker info >/dev/null 2>&1; do
-        echo "Waiting for Docker to start..."
-        sleep 1
-    done
-fi
-
 # Start supervisord
 exec /usr/bin/supervisord -c /etc/supervisord.conf
